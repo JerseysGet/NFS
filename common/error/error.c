350aaace16ee46e2a9c43cd4241d1a5a6a460cc7
JerseysGet <dev.faisal2004@gmail.com>
2023-11-08 22:39:13 +0530

    added ErrorCode type and eprintf format

--- common/error/error.c
+++ common/error/error.c
@@ -7,5 +7,6 @@
 void eprintf(const char* format, ...) {
     va_list arguments;
     va_start(arguments, format);
+    cfprintf(stderr, RED_BOLD, "[ ERROR ] ");
     cvfprintf(stderr, RED_BOLD, format, arguments);
 }
--- common/error/error.h
+++ common/error/error.h
@@ -1,6 +1,11 @@
 #ifndef __ERROR_H
 #define __ERROR_H
 
+#define FAILURE 1
+#define SUCCESS 0
+
+typedef int ErrorCode;
+
 void eprintf(const char* format, ...);
 
 #endif  
