71f5f333e368137be987a1a49d0c3d53112d2206
JerseysGet <dev.faisal2004@gmail.com>
2023-11-08 22:39:53 +0530

    added common/networking

---
+++ common/networking/networking.c
@@ -0,0 +1,86 @@
+#include "networking.h"
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+
+extern const char* GLOBAL_IP = "127.0.0.1";
+extern int CLIENT_LISTEN_PORT = 9459;
+extern int SS_LISTEN_PORT = 11764;
+
+#define MAX_CONNECTION_IN_QUEUE 10
+
+ErrorCode createPassiveSocket(int* sockfd, int port) {
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sockfd == -1) {
+        eprintf("Could not create passive socket, errno = %d, %s\n", errno, strerror(errno));
+        return FAILURE;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, '\0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(GLOBAL_IP);
+
+    if (bind(*sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        eprintf("Could not bind to port %d, errno = %d, %s\n", port, errno, strerror(errno));
+        return FAILURE;
+    }
+
+    if (listen(sockfd, MAX_CONNECTION_IN_QUEUE) == -1) {
+        eprintf("Could not listen() on port %d, errno = %d, %s\n", port, errno, strerror(errno));
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+ErrorCode createActiveSocket(int* sockfd) {
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sockfd == -1) {
+        eprintf("Could not create active socket, errno = %d, %s\n", errno, strerror(errno));
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+ErrorCode connectToServer(int clientSockfd, int serverPort) {
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = serverPort;
+    serverAddr.sin_addr.s_addr = inet_addr(GLOBAL_IP);
+
+    if (connect(clientSockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        eprintf("Could not connect to port %d, errno = %d, %s\n", serverPort, errno, strerror(errno));
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+ErrorCode acceptClient(int serverSockfd, int* clientSockfd) {
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen;
+    clientAddrLen = sizeof(clientAddr);
+    *clientSockfd = accept(serverSockfd, (struct sockarr*)&clientAddr, &clientAddrLen);
+    if (*clientSockfd == -1) {
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+ErrorCode socketSend(int sockfd, void* dataPtr, size_t bytes) {
+    if (send(sockfd, dataPtr, bytes, 0) == -1) {
+        return FAILURE;
+    }
+}
+
+ErrorCode socketRecieve(int sockfd, void* dataPtr, size_t bytes) {
+    if (recv(sockfd, dataPtr, bytes, 0) == -1) {
+        return FAILURE;
+    }
+}
--- common/networking/networking.h
+++ common/networking/networking.h
@@ -1,8 +1,31 @@
 #ifndef __NETWORKING_H
 #define __NETWORKING_H
 
+#include <stddef.h>
+
+#include "../error/error.h"
+
 extern const char* GLOBAL_IP;
+extern int CLIENT_LISTEN_PORT;
+extern int SS_LISTEN_PORT;
 
+ErrorCode createPassiveSocket(int* sockfd, int port);
+ErrorCode createActiveSocket(int* sockfd);
 
+ErrorCode connectToServer(int clientSockfd, int serverPort);
 
+/*
+    Does not print error message in case of FAILURE
+*/
+ErrorCode acceptClient(int serverSockfd, int* clientSockfd);
+
+/*
+    Does not print error message in case of FAILURE
+*/
+ErrorCode socketSend(int sockfd, void* dataPtr, size_t bytes);
+
+/*
+    Does not print error message in case of FAILURE
+*/
+ErrorCode socketRecieve(int sockfd, void* dataPtr, size_t bytes);
 #endif
