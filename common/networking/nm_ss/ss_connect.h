e5ed7b2ce092c5310a07447397072a681286ea8c
JerseysGet <dev.faisal2004@gmail.com>
2023-11-09 13:17:52 +0530

    added ss_connect.h

---
+++ common/networking/nm_ss/ss_connect.h
@@ -0,0 +1,21 @@
+#ifndef __SS_CONNECT_H
+#define __SS_CONNECT_H
+
+#include "../../error/error.h"
+
+#define MAX_PATH_COUNT 32
+#define MAX_PATH_LEN 256
+
+typedef struct AccessiblePaths {
+    int count;
+    char pathList[MAX_PATH_COUNT][MAX_PATH_LEN]; /* List of paths  */
+} AccessiblePaths;
+
+typedef struct SSInitRequest {
+    int SSPassivePort; /* NM will connect to SS on this port for communication */
+    int SSAlivePort;  /* NM will check if SS is alive on this port */ 
+    int SSClientPort; /* NM will store this port for communcation between SS and client */
+    AccessiblePaths paths;
+} SSInitRequest;
+
+#endif
