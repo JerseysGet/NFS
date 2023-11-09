648278a7603bde3c966e6eaf9a2d30a10f7d6813
JerseysGet <dev.faisal2004@gmail.com>
2023-11-09 13:19:33 +0530

    Added TODO for storage_server

---
+++ storage_server/storage_server.c
@@ -0,0 +1,13 @@
+#include "../common/networking/networking.h"
+#include "../common/networking/nm_ss/ss_connect.h"
+int main() {
+/*
+    1. Input number of paths
+    2. Input paths
+    3. Fill in the struct SSInitRequest (fill random values of ports for now)
+    4. Create an active socket
+    5. Connect to SS_LISTEN_PORT
+    6. Send the struct 
+    7. Close the socket and terminate
+*/
+}
