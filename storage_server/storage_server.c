499303a246da6de47117b0e2d8d459d350567eea
divyansh1702 <divyanshjain1702@gmail.com>
2023-11-09 20:38:11 +0530

    made some changes

--- storage_server/storage_server.c
+++ storage_server/storage_server.c
@@ -1,13 +1,61 @@
-#include "../common/networking/networking.h"
-#include "../common/networking/nm_ss/ss_connect.h"
+#include "storage_server.h"
+
+#define MAX_PATH_LENGTH 256
+struct SSInitRequest {
+    // Define the structure members 
+    int numPaths; // Replace 
+    int pathPorts[MAX_NUM_PATHS]; // Replace 
+};
+
 int main() {
-/*
-    1. Input number of paths
-    2. Input paths
-    3. Fill in the struct SSInitRequest (fill random values of ports for now)
-    4. Create an active socket
-    5. Connect to SS_LISTEN_PORT
-    6. Send the struct 
-    7. Close the socket and terminate
-*/
+    
+    int numPaths;
+    printf("Enter the number of paths: ");
+    scanf("%d", &numPaths);
+
+    
+    char paths[numPaths][MAX_PATH_LENGTH];
+    for (int i = 0; i < numPaths; i++) {
+        printf("Enter path %d: ", i + 1);
+        scanf("%s", paths[i]);
+    }
+
+   
+    struct SSInitRequest initRequest;
+    initRequest.numPaths = numPaths;
+    for (int i = 0; i < numPaths; i++) {
+        
+        initRequest.pathPorts[i] = rand() % 65536;
+    }
+
+  
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        perror("Error creating socket");
+        exit(1);
+    }
+
+    
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SS_LISTEN_PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Replace with the actual server IP address
+
+    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error connecting to server");
+        close(socket_fd);
+        exit(1);
+    }
+
+    
+    if (send(socket_fd, &initRequest, sizeof(initRequest), 0) == -1) {
+        perror("Error sending data");
+        close(socket_fd);
+        exit(1);
+    }
+
+    
+    close(socket_fd);
+    return 0;
 }
---
+++ storage_server/storage_server.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+
+#include "../common/networking/networking.h"
+#include "../common/networking/nm_ss/ss_connect.h"
