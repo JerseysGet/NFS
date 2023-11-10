822e772f14b6187ae4e8b49df1911e1c0941e61b
JerseysGet <dev.faisal2004@gmail.com>
2023-11-10 10:21:16 +0530

    added string.h to error.h

--- common/error/error.h
+++ common/error/error.h
@@ -1,6 +1,8 @@
 #ifndef __ERROR_H
 #define __ERROR_H
 
+#include <string.h>
+
 #define FAILURE 1
 #define SUCCESS 0
 
@@ -8,4 +10,4 @@ typedef int ErrorCode;
 
 void eprintf(const char* format, ...);
 
-#endif  
+#endif
