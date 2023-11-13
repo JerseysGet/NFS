a0035430d864966f5166ab3e070ea8f04bafb602
JerseysGet <dev.faisal2004@gmail.com>
2023-11-13 19:49:11 +0530

    added ui.c

---
+++ client/ui.c
@@ -0,0 +1,32 @@
+#include "../common/networking/requests/request_type.h"
+
+int main() {
+    RequestType type;
+
+}
+
+
+// int main() {
+//     // input option
+//     RequestType type;
+//     if (option == 1) type = REQUEST_WRITE;
+
+
+
+//     char path[512];
+//     // scanf oath...
+
+// }
+
+
+/*
+1. Write to File,  1 path
+2. Read a file, 1 path
+3. Delete a file 1 path
+4. Delete a folder 1 path
+5. Create a file, 1 path, 1 file name
+6. Create a folder 1 path
+7. List a folder, 1 path
+8. Get size, 1 path
+9. Get permissions, 1 path
+*/
