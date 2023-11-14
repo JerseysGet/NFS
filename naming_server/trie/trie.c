8f9e7fa4ade2263ff69a3ad10d6e00c5d7683396
JerseysGet <dev.faisal2004@gmail.com>
2023-11-14 11:11:05 +0530

    added trie

---
+++ naming_server/trie/trie.c
@@ -0,0 +1,168 @@
+#include "trie.h"
+
+#include <assert.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILE_SEPARATOR "/"
+
+typedef struct TrieNodeStruct {
+    struct TrieNodeStruct* parent;
+    struct TrieNodeStruct* next;
+    struct TrieNodeStruct* prev;
+    struct TrieNodeStruct* firstChild;
+    char* token;
+    SSInfo ssinfo;
+} TrieNodeStruct;
+
+typedef TrieNodeStruct* TrieNode;
+
+// UTIL FUNCTIONS
+
+TrieNode newNode(SSInfo ssinfo, char* token) {
+    TrieNode ret = malloc(sizeof(TrieNodeStruct));
+    assert(ret);
+    ret->parent = NULL;
+    ret->next = NULL;
+    ret->prev = NULL;
+    ret->firstChild = NULL;
+    ret->token = strdup(token);
+    ret->ssinfo = ssinfo;
+    return ret;
+};
+
+void deleteNode(TrieNode node) {
+    free(node->token);
+    free(node);
+}
+
+void link(TrieNode a, TrieNode b) {
+    if (a) a->next = b;
+    if (b) b->prev = a;
+}
+
+void appendChild(TrieNode parent, TrieNode child) {
+    assert(child);
+    assert(parent);
+    link(child, parent->firstChild);
+    parent->firstChild = child;
+    child->parent = parent;
+}
+
+// Removes itr from its links
+void removeChild(TrieNode itr) {
+    link(itr->prev, itr->next);
+    deleteNode(itr);
+}
+
+// Returns NULL if none found
+TrieNode findChild(TrieNode parent, char* token) {
+    for (TrieNode itr = parent->firstChild; itr != NULL; itr = itr->next) {
+        if (strcmp(itr->token, token)) continue;
+        return itr;
+    }
+
+    return NULL;
+}
+
+TrieNode searchNode(char* path) {
+    TrieNode parent = trie;
+    char* cpy = strdup(path);
+    char* token = strtok(cpy, FILE_SEPARATOR);
+    while (token != NULL) {
+        TrieNode child = findChild(parent, token);
+        if (child == NULL) {
+            parent = NULL;
+            goto clean_and_return;
+        }
+        parent = child;
+        token = strtok(NULL, FILE_SEPARATOR);
+    }
+clean_and_return:
+    free(cpy);
+    return parent;
+}
+
+// EXPOSED FUNCTIONS
+
+TrieNode trie = NULL;
+pthread_mutex_t lock;
+
+void initTrie() {
+    SSInfo ssinfo;
+    ssinfo.ssClientPort = -1;
+    trie = newNode(ssinfo, "");
+    pthread_mutex_init(&lock, NULL);
+}
+
+void lockTrie() {
+    pthread_mutex_lock(&lock);
+}
+
+void unlockTrie() {
+    pthread_mutex_lock(&lock);
+}
+
+ErrorCode addToTrie(char* path, SSInfo ssinfo) {
+    TrieNode find = searchNode(path);
+    if (find != NULL) return 3;
+    TrieNode parent = trie;
+    char* cpy = strdup(path);
+    char* token = strtok(cpy, FILE_SEPARATOR);
+    while (token != NULL) {
+        TrieNode child = findChild(parent, token);
+        if (child == NULL) {
+            child = newNode(ssinfo, token);
+            appendChild(parent, child);
+        }
+        parent = child;
+        token = strtok(NULL, FILE_SEPARATOR);
+    }
+    free(cpy);
+    return SUCCESS;
+}
+
+ErrorCode search(char* path, SSInfo* ret) {
+    TrieNode node = searchNode(path);
+    if (node)
+        *ret = node->ssinfo;
+    else
+        return 1;
+
+    return SUCCESS;
+}
+
+ErrorCode deleteFromTrie(char* path) {
+    TrieNode node = searchNode(path);
+    if (node == NULL) {
+        return 1;
+    }
+    if (node->firstChild != NULL) {
+        return 2;
+    }
+
+    TrieNode parent = node->parent;
+    if (parent->firstChild == node) {
+        parent->firstChild = node->next;
+    }
+
+    removeChild(node);
+    return SUCCESS;
+}
+
+void destroyTrieHelper(TrieNode node) {
+    if (node == NULL) return;
+    for (TrieNode itr = node->firstChild; itr != NULL; itr = itr->next) {
+        destroyTrieHelper(itr);
+        assert(itr == NULL);
+    }
+
+    deleteNode(node);
+}
+
+void destroyTrie() {
+    pthread_mutex_destroy(&lock);
+    destroyTrieHelper(trie);
+}
--- naming_server/trie/trie.h
+++ naming_server/trie/trie.h
@@ -1,8 +1,40 @@
 #ifndef __TRIE_H
 #define __TRIE_H
 
-void initTrie();
-void addToTrie(char* path);
+#include <stdbool.h>
 
+#include "../../common/error/error.h"
+#include "../../common/networking/ack/ssinfo.h"
+
+void initTrie();
+
+void lockTrie();
+
+void unlockTrie();
+
+/*
+    return 3 = path already exists
+    Must be externally synchronized
+*/
+ErrorCode addToTrie(char* path, SSInfo ssinfo);
+
+/*
+    return 1 = path not found
+    Must be externally synchronized
+*/
+ErrorCode search(char* path, SSInfo* ret);
+
+/*
+    return 1 = path not found
+    return 2 = path isn't empty
+    return 0 = success
+    Must be externally synchronized
+*/
+ErrorCode deleteFromTrie(char* path);
+
+/*
+    Should have exactly one thread calling this, and no threads waiting on lock. Should not hold the lock while calling destroyTrie()
+*/
 void destroyTrie();
+
 #endif
