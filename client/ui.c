07cd8aa0354e6621e557976ad333089411069dd2
divyansh1702 <divyanshjain1702@gmail.com>
2023-11-18 15:44:28 +0530

    made some changes

--- client/ui.c
+++ client/ui.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <pthread.h>
 #include "../common/networking/requests/request_type.h"
 #include "../common/networking/networking.h"
+
+// Function that each thread will execute
+void* clientHandler(void* arg) {
+    int clientSocket = *((int*)arg);
+    
+  
+    closeSocket(clientSocket);
+
+    return NULL;
+}
+
 int main() {
-    // input option
-
-    /*
-        given a socketfd to a server socket
-        create a thread that accepts clients, then disconnects those clients
-    */
-
     int port = 5555;
     int sockfd;
     createPassiveSocket(&sockfd, port);
-    // given socket = sockfd
-    
+
+    while (1) {
+        
+        int clientSocket;
+        if (acceptClient(sockfd, &clientSocket) == 0) {
+           
+            pthread_t thread;
+            pthread_create(&thread, NULL, clientHandler, (void*)&clientSocket);
+
+            /
+            pthread_detach(thread);
+        }
+    }
+
+    // Close the server socket (not reached in this example)
+    closeSocket(sockfd);
+
+    return 0;
 }
